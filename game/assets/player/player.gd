53b1ddb942852347e38bdb97a036a95f7a3145f4
tortallica <torandreasaaseboe@gmail.com>
2017-10-31 11:13:02 +0100

    elevator anim

--- game/assets/player/player.gd
+++ game/assets/player/player.gd
@@ -131,7 +131,8 @@ func _input(event):
 		get_node(PATH_CAMERA).rotate_x(deg2rad(pitch))
 	
 	if (event.type == InputEvent.MOUSE_BUTTON):
-		pass
+		get_node("../Door/AnimationPlayer").play("Open Door")
+		get_node("../Door1/AnimationPlayer").play("Open Door2")
 	# Handles key events besides the player movement
 	if (event.type == InputEvent.KEY):
 		if is_in_menu:
