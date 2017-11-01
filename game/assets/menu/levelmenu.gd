b9fe3ddc8ddf4b560176fcb6b025b71c71003e31
olav89 <olav89@live.no>
2017-11-01 15:18:15 +0100

    loading bar

--- game/assets/menu/levelmenu.gd
+++ game/assets/menu/levelmenu.gd
@@ -1,13 +1,15 @@
 #
 # Script for the level menu
-# 
-#
+# The loading is done as the tutorial at: 
+# http://docs.godotengine.org/en/latest/learning/features/misc/background_loading.html
 
 extends TextureFrame
 
 const PATH_LEVELS = "res://assets/levels/Level"
 
 var num_levels = 30
+
+var loader
 
 func _ready():
 	var box = get_node("Levels") # Vertical Box Container
@@ -20,4 +22,42 @@ func _ready():
 # Event for loading a new scene
 func button_pressed(pressed):
 	var btn_text = pressed.get_text()
-	get_tree().change_scene(PATH_LEVELS + btn_text + ".tscn")
+	goto_scene(PATH_LEVELS + btn_text + ".tscn")
+	
+func goto_scene(path): # game requests to switch to this scene
+	loader = ResourceLoader.load_interactive(path)
+	if loader == null: # check for errors
+		print("Loading failed: " + path)
+		return
+	set_process(true)
+	get_node("ProgressBar").show()
+	
+
+func _process(time):
+	if loader == null:
+		# no need to process anymore
+		set_process(false)
+		return
+	# poll your loader
+	var err = loader.poll()
+	
+	if err == ERR_FILE_EOF: # load finished
+		var resource = loader.get_resource()
+		loader = null
+		set_new_scene(resource)
+	elif err == OK:
+		update_progress()
+	else: # error during loading
+		print("Error during loading")
+		loader = null
+
+func update_progress():
+	var progress = 100*(float(loader.get_stage() + 1) / float(loader.get_stage_count()))
+	get_node("ProgressBar").set_value(progress)
+
+func set_new_scene(scene_resource):
+	var current = get_tree().get_root().get_child( get_tree().get_root().get_child_count() -1 )
+	current.queue_free()
+	current = scene_resource.instance()
+	get_tree().get_root().add_child(current)
+	get_tree().set_current_scene(current)
