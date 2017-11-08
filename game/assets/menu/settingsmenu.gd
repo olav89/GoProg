af88cf88e5b8ee52f0a37faf27513ed7f0f13e88
tortallica <torandreasaaseboe@gmail.com>
2017-11-08 18:14:08 +0100

    better sound interface

--- game/assets/menu/settingsmenu.gd
+++ game/assets/menu/settingsmenu.gd
@@ -8,6 +8,7 @@ func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
 	get_node("TextureFrame/volumeSlider").set_value(get_parent().getSoundVolume())
+	get_node("TextureFrame/volInlbl").set_text(str(get_node("TextureFrame/volumeSlider").get_value()))
 
 func _show():
 	show()
@@ -25,8 +26,13 @@ func _on_sounds_toggled( pressed ):
 func _on_sounds_pressed():
 	if(get_node("TextureFrame/sounds").is_pressed()):
 		get_parent().enableSound()
+		get_node("TextureFrame/volumeSlider").set_value(100)
+		get_node("TextureFrame/volInlbl").set_text("100")
 	else:
 		get_parent().disableSound()
+		get_node("TextureFrame/volumeSlider").set_value(0)
+		get_node("TextureFrame/volInlbl").set_text("0")
 
 func _on_volumeSlider_value_changed( value ):
 	get_parent().setSoundVolume(float(value/100))
+	get_node("TextureFrame/volInlbl").set_text(str(value))
