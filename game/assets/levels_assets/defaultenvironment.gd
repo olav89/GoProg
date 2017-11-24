6097d28a5538a50ff2616d93a9ef438c4f15a621
tortallica <torandreasaaseboe@gmail.com>
2017-11-24 09:14:21 +0100

    fix

--- game/assets/levels_assets/defaultenvironment.gd
+++ game/assets/levels_assets/defaultenvironment.gd
@@ -78,16 +78,13 @@ func gravity_timer_finished():
 func won():
 	is_level_won = true
 	get_node(PATH_PLAYER).journal.change_journal("Level complete. Proceed to the elevator.")
-<<<<<<< HEAD
 	get_node(PATH_PLAYER).get_node("SamplePlayer").play_sample_ding()
-	
-=======
 	if get_node(DEFAULT + "Door").has_method("open"):
 		get_node(DEFAULT + "Door").open()
 		get_node(DEFAULT + "Door1").open()
 	else:
 		get_node("/root/logger").log_error("Doors does not have method open")
->>>>>>> cabf2e49ccb6c2fccc7a114aaae48b90936b6cc6
+
 
 func is_won():
 	return is_level_won
