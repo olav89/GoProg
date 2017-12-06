5f997f39866b236ae9beefc7aa7a731799a6c21d
tortallica <torandreasaaseboe@gmail.com>
2017-12-06 21:38:43 +0100

    added Door.tscn

--- game/assets/tv/tv.gd
+++ game/assets/tv/tv.gd
@@ -2,10 +2,12 @@ extends Quad
 
 signal finished
 
+
 func _ready():
 	#get_material_override().set_texture(FixedMaterial.PARAM_DIFFUSE, get_node("TextureFrame/Viewport").get_render_target_texture())
 	var rtt = get_node("Viewport").get_render_target_texture()
 	get_material_override().set_texture(FixedMaterial.PARAM_DIFFUSE,rtt) 
+
 
 func update_rtt():
 	var rtt = get_node("Viewport").get_render_target_texture()
