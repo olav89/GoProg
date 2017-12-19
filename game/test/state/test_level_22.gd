10fa7e30043c1175440bd625520a708f77d8ce36
olav89 <olav89@live.no>
2017-12-19 14:36:47 +0100

    added state test

---
+++ game/test/state/test_level_22.gd
@@ -0,0 +1,26 @@
+extends "res://addons/gut/test.gd"
+
+var level = load("res://assets/levels/Level22.tscn").instance()
+
+func setup():
+	gut.p("ran setup", 2)
+	
+func teardown():
+	gut.p("ran teardown", 2)
+
+func prerun_setup():
+	gut.p("ran run setup", 2)
+	get_tree().get_root().add_child(level)
+	get_node("/root/logger").set_print(false)
+
+func postrun_teardown():
+	gut.p("ran run teardown", 2)
+
+func test_win():
+	var code = """invert_gravity_room()
+angle_cannon(6)
+fire_cannon()"""
+	level.get_node(level.PATH_PC).get_screen().set_editor_text(code)
+	get_node("/root/execute").execute_code()
+	yield(yield_for(8), YIELD)
+	assert_true(level.is_won())
