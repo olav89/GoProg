8d529071e435cffef7c19c0504321468b9a899e1
tortallica <torandreasaaseboe@gmail.com>
2017-12-25 13:55:50 +0100

    restart lvl 13 for something worth keeping

--- game/assets/levels/Level13.gd
+++ game/assets/levels/Level13.gd
@@ -8,11 +8,7 @@ func _ready():
 	journal_text += "- Interact with the PC by pressing E.\n"
 	# setup scripts
 	run_setup()
-	get_node(PATH_PLAYER).set_translation(Vector3(-15.0,1.0,15.0))
-	get_node(PATH_PLAYER).set_rotation(Vector3(0.0,0.0,0.0))
-	get_node("defaultenviroment/Table").set_translation(Vector3(-13.0,0.0,15.0))
-	get_node(PATH_PC).set_translation(Vector3(-12.0,1.8,14.5))
-	get_node(PATH_CRATE).set_scale(Vector3(2.0,2.0,2.0))
+	
 	
 func _process(delta):
 	#get_node(PATH_PLAYER)
