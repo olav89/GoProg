be019998e3622a6caf6f9b26e65c4cd41f8a2974
olav89 <olav89@live.no>
2017-12-26 18:00:56 +0100

    removed player test, added text on execute tests

--- game/test/unit/test_execute.gd
+++ game/test/unit/test_execute.gd
@@ -25,29 +25,30 @@ func test_get_error_information():
 if 2.3 print
 	print(2)""")
 	var t2 = execute.get_error_information("print(1)")
-	assert_true(t1.find("Line 2") > -1)
-	assert_true(t2.find("Line 1") == -1)
+	assert_true(t1.find("Line 2") > -1, "%s should give parse error on line 2" % t1)
+	assert_true(t2.find("Line 1") == -1, "%s should not give any parse error" % t2)
 
 func test_parser_oneliners():
 	var func_def = "func test():\n\t"
-	assert_eq(execute.parse(func_def + "print(20)")[0], OK) # func calls
-	assert_eq(execute.parse(func_def + "var a = 2")[0], OK) # number vars
-	assert_eq(execute.parse(func_def + "var b = \"test\"")[0], OK) # string vars
-	assert_eq(execute.parse(func_def + "var a = some_func()")[0], OK) # regression
+	assert_eq(execute.parse(func_def + "print(20)")[0], OK, "simple print call parse = OK") 
+	assert_eq(execute.parse(func_def + "var a = 2")[0], OK, "simple int declaration parse = OK")
+	assert_eq(execute.parse(func_def + "var b = \"test\"")[0], OK, "simple string declaration parse = OK") 
+	assert_eq(execute.parse(func_def + "var a = some_func()")[0], OK, "function call parse = OK")
 	
-	assert_eq(execute.parse(func_def + "var a = some _func()")[0], ERR_PARSE) # wrong space
-	assert_eq(execute.parse(func_def + func_def)[0], ERR_PARSE) # func inside func
+	assert_eq(execute.parse(func_def + "var a = some _func()")[0], ERR_PARSE, "space error in function call parse = ERR") 
+	assert_eq(execute.parse(func_def + func_def)[0], ERR_PARSE, "function inside function with no body parse = ERR") 
 
 func test_match_custom_func():
-	assert_ne(execute.match_custom_func("invert_gravity_room()"), "")
-	assert_ne(execute.match_custom_func("invert_gravity_player()"), "")
-	assert_ne(execute.match_custom_func("move_crate_left()"), "")
-	assert_ne(execute.match_custom_func("move_crate_right()"), "")
-	assert_ne(execute.match_custom_func("move_crate_forward()"), "")
-	assert_ne(execute.match_custom_func("move_crate_backward()"), "")
-	assert_ne(execute.match_custom_func("light_switch()"), "")
-	assert_ne(execute.match_custom_func("fire_cannon()"), "")
-	assert_ne(execute.match_custom_func("angle_cannon()"), "")
+	var text = "custom function matching should give a nonempty string"
+	assert_ne(execute.match_custom_func("invert_gravity_room()"), "", text)
+	assert_ne(execute.match_custom_func("invert_gravity_player()"), "", text)
+	assert_ne(execute.match_custom_func("move_crate_left()"), "", text)
+	assert_ne(execute.match_custom_func("move_crate_right()"), "", text)
+	assert_ne(execute.match_custom_func("move_crate_forward()"), "", text)
+	assert_ne(execute.match_custom_func("move_crate_backward()"), "", text)
+	assert_ne(execute.match_custom_func("light_switch()"), "", text)
+	assert_ne(execute.match_custom_func("fire_cannon()"), "", text)
+	assert_ne(execute.match_custom_func("angle_cannon()"), "", text)
 
 # Extracting function names from code array
 func test_make_func_names():
@@ -55,7 +56,7 @@ func test_make_func_names():
 		"func wrong", "func testingB():"]
 	execute.make_func_names(code)
 	var expected = ["testA", "testingB"]
-	assert_eq(execute.func_names, expected, "test_make_func_names")
+	assert_eq(execute.func_names, expected, "only proper function definitions should be accepted")
 
 # Test that make_function separates code correctly
 func test_make_function_simple():
@@ -69,7 +70,7 @@ func test_make_function_simple():
 	
 	var eval_str = execute.make_function(input)
 	assert_true(eval_str.match("*func eval():*test()*func test():*print(2)*"),
-	"test_make_simple_function")
+	"eval() must contain all code not inside a function")
 
 func test_make_function_yield():
 	execute.func_names = ["test"]
@@ -86,7 +87,7 @@ func test():
 	print()
 	emit_signal("test_finished")
 """
-	assert_eq(res, expected)
+	assert_eq(res, expected, "functions without return must have yields and emits")
 
 func test_make_function_return():
 	execute.func_names = ["test"]
@@ -101,7 +102,7 @@ func eval():
 func test():
 	return 2
 """
-	assert_eq(res, expected)
+	assert_eq(res, expected, "functions with return does not have yields and emits")
 
 func test_leading_spaces():
 	assert_eq(execute.leading_spaces("  twospaces"), "  ", "two spaces")
