dabf5d8e4f413027a2acb418467ff4f7dfc1180f
olav89 <olav89@live.no>
2017-12-26 14:07:00 +0100

    logger now writes with buffer

--- game/assets/logger/logger.gd
+++ game/assets/logger/logger.gd
@@ -6,6 +6,12 @@ extends Node
 
 # Output file
 var file
+var fname = "res://log.log"
+var mode = File.WRITE
+
+# Buffer
+var buffer = [10]
+var b_ind = 0
 
 # Options
 var debug = false
@@ -14,9 +20,11 @@ var print_log = false
 # Opens file
 func _ready():
 	file = File.new()
-	file.open("res://log.log", File.WRITE)
+	file.open(fname, mode)
 	if file != null:
 		log_info("Init...")
+		file.close()
+		mode = File.READ_WRITE # append new writes
 	set_debug(true)
 	set_print(true)
 
@@ -29,24 +37,36 @@ func set_print(enable):
 # Called when quitting
 func _exit_tree():
 	log_info("Quit...")
-	file.close() # close file
+	write_buffer_to_file()
 
-# Writes a string to file
-func write_to_file(s):
+# Writes buffer to file
+func write_buffer_to_file():
+	file.open(fname, mode)
 	if file != null:
-		file.store_string(s + "\n")
+		file.seek_end()
+		for i in range(b_ind):
+			file.store_string(buffer[i] + "\n")
+		file.close()
+	b_ind = 0
+
+# Adds a string to the buffer
+func append_buffer(s):
+	buffer[b_ind] = s
+	b_ind += 1
+	if b_ind == buffer.size():
+		write_buffer_to_file()
 	if print_log:
 		print(s)
 
 # All log functions
-# Each adds a tag to input before writing it to file
+# Each adds a tag to input
 
 func log_info(s):
-	write_to_file("INFO:" + s)
+	append_buffer("INFO:" + s)
 func log_warning(s):
-	write_to_file("WARNING:" + s)
+	append_buffer("WARNING:" + s)
 func log_debug(s):
 	if debug:
-		write_to_file("DEBUG:" + s)
+		append_buffer("DEBUG:" + s)
 func log_error(s):
-	write_to_file("ERROR:" + s)
+	append_buffer("ERROR:" + s)
