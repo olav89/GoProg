f34f3fb82a747b2a283b52a3543da49eee6a228b
tortallica <torandreasaaseboe@gmail.com>
2018-01-03 17:21:01 +0100

    lvl 16

--- game/assets/levels/Level16.gd
+++ game/assets/levels/Level16.gd
@@ -1,10 +1,45 @@
-extends Spatial
+extends "res://assets/levels_assets/defaultenvironment.gd"
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	# setup variables
+	set_process(true)
+	PATH_CRATE = "Crate"
+	PATH_CANNON = "Cannon"
+	PATH_LIGHTBOARD = "Lightboard"
+	journal_text = "- Access the PC and learn about variables\n"
+	journal_text += "- Interact with the PC by pressing E.\n"
+	
+	
+	editor_text = """   
+#Here you can test out the effect diffrent variables have on items. 
+#The Variables a - d are imputs in diffrent functions. see for yourself
+	
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+
+for i in range(d):
+	light_switch(i)
+
+fire_cannon(c)
+
+move_crate_forward(b)
+move_crate_back(a)
+
+#when you are ready to try to advance to the next lvl, 
+#manipulate the variables so that the crates moves in front
+#of the gun and shoot at it
+	"""
+	
+	# setup scripts
+	run_setup()
+	
+	
+func _process(delta):
 	pass
+
+func cannon_hit(body):
+	if get_node(PATH_CRATE).is_crate(body):
+		won()
