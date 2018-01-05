6c9017a2d96e4dee9f2354b88a7fa709512d9e74
tortallica <torandreasaaseboe@gmail.com>
2018-01-05 09:48:12 +0100

    brushup lvl var_1

--- game/assets/levels/var_1.gd
+++ game/assets/levels/var_1.gd
@@ -10,14 +10,24 @@ func _ready():
 	set_process(true)
 	PATH_TV = DEFAULT + "Tv"
 	PATH_CRATE = "Crate"
-	journal_text = "- Find the green button and press it.\n"
+	journal_text = "- Solve Math on Wall.\n"
 	journal_text += "- Interact with the PC by pressing E.\n"
 	
 	TEXT_TV = " a + b = 5 \n a>0 \n b>0 \n a= \n b= "
 	get_node("defaultenviroment/Tv/Viewport/TextureFrame/Label").set_text(TEXT_TV)
+	editor_text = """
+	# use set_a(?) and set_b(?) and exchange the ? with your answers for a and b 
+	# press either build and then exit the computer and rund the code with f-button 
+	# or just hit the execute button
+	
+	set_a(?)
+	set_b(?)
+	"""
 	
 	# setup scripts
 	run_setup()
+	
+
 
 func set_text_tv(text):
 	get_node("defaultenviroment/Tv/Viewport/TextureFrame/Label").set_text(text)
